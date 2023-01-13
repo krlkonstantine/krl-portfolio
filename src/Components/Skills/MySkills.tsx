af130d7aa42aabd4fca59cfdb28e1531dd957861
krlkonstantine <kostea.023@gmail.com>
2023-01-13 11:02:00 +0300

    Skills section added

--- src/Components/Skills/MySkills.tsx
+++ src/Components/Skills/MySkills.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import MySkillsContainer from './MySkills.module.css'
-import {Skill} from "./Skill/Skill";
-import stylesContainer from "../../common/styles/Container.module.css"
+import {Skill, SkillType} from "./Skill/Skill";
 
 
 export const MySkills = () => {
+
+    const MySkills: SkillType[] = [
+        {
+            skillImg: "https://icons-for-free.com/iconfiles/png/512/design+development+facebook+framework+mobile+react+icon-1320165723839064798.png",
+            skillTitle: "React",
+            skillDescription: "Реакт Редакс ты знать будешь круто...Айти кама айти кама, камасутрааааааа! Реакт Редакс ты знать будешь круто..."
+        },
+        {
+            skillImg: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/redux-icon.png",
+            skillTitle: "Redux",
+            skillDescription: "Реакт Редакс ты знать будешь круто...Айти кама айти кама, камасутрааааааа! Реакт Редакс ты знать будешь круто..."
+        },
+        {
+            skillImg: "https://cdn1.iconfinder.com/data/icons/soleicons-solid-vol-1/64/reactjs_javascript_library_atom_atomic_react-512.png",
+            skillTitle: "React Native",
+            skillDescription: "Реакт Редакс ты знать будешь круто...Айти кама айти кама, камасутрааааааа! Реакт Редакс ты знать будешь круто..."
+        }
+    ]
     return (
         <div className={MySkillsContainer.mainSkills}>
-            <div className={stylesContainer.basicContainer}>
-            <span className={MySkillsContainer.skillsTitle}>
-            </span>
-                <Skill/>
+            <div className={MySkillsContainer.skillsSpecialContainer}>
+                <h2 className={MySkillsContainer.skillsTitle}>
+                    Skills
+                </h2>
+                <div className={MySkillsContainer.skillsContainer}>
+                    {MySkills.map( skill =>
+                        <Skill skillImg={skill.skillImg} skillTitle={skill.skillTitle} skillDescription={skill.skillDescription}/>
+                    )}
+                </div>
+
             </div>
         </div>
     );
--- src/Components/Skills/Skill/Skill.tsx
+++ src/Components/Skills/Skill/Skill.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import skillStyle from './Skill.module.css'
 
-export const Skill = () => {
+export type SkillType = {
+    skillImg: string
+    skillTitle: string
+    skillDescription: string
+}
+
+export const Skill = (props: SkillType) => {
     return (
         <div className={skillStyle.skillContainer}>
-            <div className={skillStyle.skillImg}>
-
-            </div>
-            <div className={skillStyle.skillDescription}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, voluptate?
-            </div>
+            <div className={skillStyle.iconContainer}>
+                <img className={skillStyle.skillIcon} src={props.skillImg} alt={props.skillTitle}/></div>
+            <h3 className={skillStyle.skillTitle}>{props.skillTitle}</h3>
+            <span className={skillStyle.skillDescription}>
+                {props.skillDescription} </span>
         </div>
     );
 };
