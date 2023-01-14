86844b54fdfcf987e448d3f10f64f74ea0f8fbbf
krlkonstantine <kostea.023@gmail.com>
2023-01-14 18:06:41 +0300

    some styling changes

--- src/Components/MyWork/CertainWork/CertainWork.tsx
+++ src/Components/MyWork/CertainWork/CertainWork.tsx
@@ -12,9 +12,15 @@ export const CertainWork = (props: WorksType) => {
     return (
         <div className={certainWorkStyle.certainWorkContainer}>
             <div className={certainWorkStyle.iconContainer}>
-                <img className={certainWorkStyle.certainWorkIcon} src={props.workImg} alt={props.skillTitle}/>
-                <button className={certainWorkStyle.watchWorkBtn}>Watch</button>
-            </div>
+                {/*<img className={certainWorkStyle.certainWorkIcon} src={props.workImg} alt={props.skillTitle}/>
+                initially the images were displayed through position property
+                the links were received via props. I decided to keep this old code
+                as this topic will be in the second lesson
+                If you're still reading this then thank you so much for cod review =3
+                */}
+                <span><a className={certainWorkStyle.watchWorkBtn}>Watch</a>
+            </span>
+                </div>
             <h3 className={certainWorkStyle.certainWorkTitle}>{props.skillTitle}</h3>
             <span className={certainWorkStyle.certainWorkDescription}>
                 {props.skillDescription} </span>
--- src/Components/Skills/MySkills.tsx
+++ src/Components/Skills/MySkills.tsx
@@ -9,17 +9,17 @@ export const MySkills = () => {
         {
             skillImg: "https://icons-for-free.com/iconfiles/png/512/design+development+facebook+framework+mobile+react+icon-1320165723839064798.png",
             skillTitle: "React",
-            skillDescription: "Реакт Редакс ты знать будешь круто."
+            skillDescription: "Реакт Редакс ты знать будешь круто...Айти кама айти кама, камасутрааааааа! Реакт Редакс ты знать будешь круто..."
         },
         {
             skillImg: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/redux-icon.png",
             skillTitle: "Redux",
-            skillDescription: "Айти кама айти кама, камасутрааааааа!"
+            skillDescription: "Реакт Редакс ты знать будешь круто...Айти кама айти кама, камасутрааааааа! Реакт Редакс ты знать будешь круто..."
         },
         {
             skillImg: "https://cdn1.iconfinder.com/data/icons/soleicons-solid-vol-1/64/reactjs_javascript_library_atom_atomic_react-512.png",
             skillTitle: "React Native",
-            skillDescription: "Реакт Редакс ты знать будешь круто!"
+            skillDescription: "Реакт Редакс ты знать будешь круто...Айти кама айти кама, камасутрааааааа! Реакт Редакс ты знать будешь круто..."
         }
     ]
     return (
