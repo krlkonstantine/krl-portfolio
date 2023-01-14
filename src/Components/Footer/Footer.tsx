7423eab92229411ad7c2286347540d5b7581e33a
krlkonstantine <kostea.023@gmail.com>
2023-01-14 16:24:15 +0300

    small refactoring

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -9,29 +9,29 @@ export const Footer = () => {
                     Konstantine Kornitsel
                 </div>
                 <div className={stylesFooter.sNetworksImgLinksContainer}>
-                    <a href="https://www.facebook.com/it.incubator.by/"><img className={stylesFooter.sNetworksImgs}
+                    <a href="https://www.facebook.com/krlkostya/" target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                              src="https://louisville.edu/english/images/facebookicon.png/image"
                                                                              alt="fb-logo"/></a>
 
 
-                    <a href="https://vk.com/kornitsel/"><img className={stylesFooter.sNetworksImgs}
+                    <a href="https://vk.com/kornitsel/" target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                              //src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/512px-Twitter-logo.svg.png"
                                                                              src="https://cdn-icons-png.flaticon.com/512/2111/2111747.png"
                                                                              //src="../vk_logo_2.png"
                                                                              alt="vk-logo"/></a>
                     <a
-                        href="https://t.me/kornitsel/"><img className={stylesFooter.sNetworksImgs}
+                        href="https://t.me/kornitsel/" target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                               src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png"
                                                                               alt="tg-logo"/></a>
 
 
-                    <a href="https://www.facebook.com/it.incubator.byhttps://github.com/krlkonstantine/"><img className={stylesFooter.sNetworksImgs}
+                    <a href="https://github.com/krlkonstantine/" target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                              src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"
                                                                              alt="gitHub-logo"/></a>
 
                 </div>
                 <span className={stylesFooter.frrelanceBtn}>
-                    Send Message
+                    Copyright © 2023 KornitselDev. All rights reserved.
                 </span>
 
             </div>
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import homeStyle from './Home.module.css'
-import stylesContainer from "../../common/styles/Container.module.css"
 
 export const Home = () => {
     return (
         <div className={homeStyle.mainHome}>
-            <div className={stylesContainer.basicContainer}>
+            <div className={homeStyle.basicContainer}>
                 <div className={homeStyle.greetingText}>
                     <span>Hi there!</span>
                     <h1>I am Konstantine Kornitsel</h1>
                     <p>A Frontend developer</p>
                 </div>
                 <div className={homeStyle.myPhoto}>
-
+{/*
+                    <img className={homeStyle.img} src="https://avatars.githubusercontent.com/u/82206018?v=4" alt=""/>
+*/}
                 </div>
             </div>
         </div>
