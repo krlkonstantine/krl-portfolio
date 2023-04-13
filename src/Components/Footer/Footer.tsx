27cfb42798479cd38bd02987914c0e87b6e55ab1
krlkonstantine <kostea.023@gmail.com>
2023-04-13 13:54:17 +0300

    styles applied for all components

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
-import stylesFooter from './Footer.module.css'
+import stylesFooter from './Footer.module.scss'
 
 export const Footer = () => {
+
+    let socialMediaLinks = {
+        fb: "https://www.facebook.com/krlkostya/",
+        vk: "https://vk.com/kornitsel/",
+        tg: "https://t.me/kornitsel/",
+        gt: "https://github.com/krlkonstantine/",
+    }
+
     return (
         <div className={stylesFooter.footerContainer}>
-            <div className={stylesFooter.basicContainer}>
+            <div className={stylesFooter.footerContentContainer}>
                 <div className={stylesFooter.displText}>
-                    Konstantine Kornitsel
+                    Konstantine
                 </div>
                 <div className={stylesFooter.sNetworksImgLinksContainer}>
-                    <a href="https://www.facebook.com/krlkostya/" target="_blank"><img className={stylesFooter.sNetworksImgs}
+                    <a href={socialMediaLinks.fb} target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                              src="https://louisville.edu/english/images/facebookicon.png/image"
                                                                              alt="fb-logo"/></a>
-                    <a href="https://vk.com/kornitsel/" target="_blank"><img className={stylesFooter.sNetworksImgs}
+                    <a href={socialMediaLinks.vk} target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                              src="https://cdn-icons-png.flaticon.com/512/2111/2111747.png"
                                                                              alt="vk-logo"/></a>
-                    <a href="https://t.me/kornitsel/" target="_blank"><img className={stylesFooter.sNetworksImgs}
+                    <a href={socialMediaLinks.tg} target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                               src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png"
                                                                               alt="tg-logo"/></a>
-                    <a href="https://github.com/krlkonstantine/" target="_blank"><img className={stylesFooter.sNetworksImgs}
+                    <a href={socialMediaLinks.gt} target="_blank"><img className={stylesFooter.sNetworksImgs}
                                                                              src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"
                                                                              alt="gitHub-logo"/></a>
 
                 </div>
-                <span className={stylesFooter.frrelanceBtn}>
+                <span className={stylesFooter.copyryghtText}>
                     Copyright © 2023 KornitselDev. All rights reserved.
                 </span>
 
