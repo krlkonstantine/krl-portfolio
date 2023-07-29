53d1e626ee5d33bcf8a2471a7a5bcc929902c547
krlkonstantine <kostea.023@gmail.com>
2023-07-29 09:46:45 +0300

    react tilt implemented and applied to photo

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import homeStyle from './Home.module.scss'
 import {TsParticles} from "../common/Particles/TsParticles";
-import {Slide } from "react-awesome-reveal";
-import ReactTypingEffect from "react-typing-effect";
+import {Slide} from "react-awesome-reveal";
 import Typewriter from 'react-ts-typewriter';
-
+import {Tilt} from 'react-tilt'
+import {defaultOptions} from "../common/ReactTilt/react-tilt-params";
 
 
 class Properties<T, U> {
@@ -23,17 +23,21 @@ export const Home = (props: AboutMePropsType) => {
                     <span className={homeStyle.mainHello}>Hello there!</span>
                     <h1>My name is <span>Konstantine</span></h1>
                     <p className={homeStyle.shortPrestext}>
-                        <Typewriter delay={30} random={50} speed={60} text={"I'm a Frontend developer, working with React-Redux"}/>
-                        </p>
+                        <Typewriter delay={30} random={50} speed={60}
+                                    text={"I'm a Frontend developer, working with React-Redux"}/>
+                    </p>
                 </div>
+
                 <Slide direction={"right"} duration={1200}>
                     <div className={homeStyle.greetingImgContainer}>
-                        <div style={props.style} className={homeStyle.myImg}>
-
-                        </div>
+                        <Tilt options={defaultOptions}>
+                            <div style={props.style} className={homeStyle.myImg}>
+                            </div>
+                        </Tilt>
                     </div>
-                </Slide >
-                <TsParticles />
+                </Slide>
+
+                <TsParticles/>
             </div>
 
         </div>
